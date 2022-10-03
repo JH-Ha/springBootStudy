f4a3c0c19260c168eeb7a258a5bf2d0f8e35a215
JH-Ha <cubeprince@gmail.com>
2022-10-03 13:04:28 +0900

    apply HelloTraceV2 to V2 codes

---
+++ src/main/java/hello/advanced/app/v2/OrderRepositoryV2.java
@@ -0,0 +1,37 @@
+package hello.advanced.app.v2;
+
+import hello.advanced.trace.TraceId;
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.helloTraceV1.HelloTraceV2;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV2 {
+
+  private final HelloTraceV2 trace;
+
+  public void save(TraceId traceId, String itemId) {
+    TraceStatus status = trace.beginSync(traceId, "OrderRepository.save()");
+    try {
+      if (itemId.equals("ex")) {
+        throw new IllegalStateException("exception !!");
+      }
+      sleep(1000);
+      trace.end(status);
+    } catch (Exception e) {
+      trace.exception(status, e);
+      throw e;
+    }
+  }
+
+  private void sleep(int millis) {
+    try {
+      Thread.sleep(millis);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+}
